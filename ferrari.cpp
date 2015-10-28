b555f2769845003b0e65f9f21f0a435afca0020f
Aluno <Aluno@LAB1-PC08>
2015-10-27 22:59:35 -0200

    iiii

--- ferrari.cpp
+++ ferrari.cpp
@@ -1,10 +1,37 @@
-#include "ferrari.h"
+#include "Ferrari.h"
 
-ferrari::ferrari()
+
+Ferrari::Ferrari()
 {
+    this->aceleracao=7.8;
+    this->velocidade=0;
+    this->estado=0;
+
 }
 
-ferrari::~ferrari()
+Ferrari::Ferrari(double aceleracao, double velocidade, int estado)
 {
+   this->aceleracao=aceleracao;
+   this->velocidade=velocidade;
+   this->estado=estado;
+
 }
+
+Ferrari::ligar(int ponto){
+
+    this->estado=ponto;
+
+}
+
+Ferrari::acelerar(double segundos){
+
+    this->aceleracao=aceleracao*segundos;
+}
+
+Ferrari::reduzir(double freio){
+
+    this->velocidade=velocidade-freio;
+}
+
+~Ferrari::Ferrari()
 
--- ferrari.h
+++ ferrari.h
@@ -1,11 +1,22 @@
 #ifndef FERRARI_H
 #define FERRARI_H
+#include<string>
 
-class ferrari
+class Ferrari
 {
 public:
-	ferrari();
-	~ferrari();
+	Ferrari();
+	Ferrari(double , double, int);
+
+	void reduzir(double);
+	void acelerar(double);
+	void setEstado(bool);
+
+private:
+
+	double velocidadeAtual;
+	double kilometroPercorrido;
+	bool estado;
 
 };
 
